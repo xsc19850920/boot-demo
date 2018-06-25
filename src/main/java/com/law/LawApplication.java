885dc9eaa8ad186454c9caf0816c76fe0a7523ac
xsc19850920 <xsc19850920@163.com>
2018-06-25 18:49:01 +0800

    init

---
+++ src/main/java/com/law/LawApplication.java
@@ -0,0 +1,12 @@
+package com.law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawApplication.class, args);
+	}
+}
